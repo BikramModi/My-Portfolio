05e4a9c3f7f0e607430623f0cf56f645445da31f
Bikram Modi <bikrammodi132@gmail.com>
2026-05-22 11:58:13 +0545

    server and db setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import express from "express";
+import connectDB from "./config/database.js";
+
+const SERVER = express();
+
+connectDB();
 
 
+const PORT = process.env.PORT;
 
 
+SERVER.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  
+});
