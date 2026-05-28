08ba83fefd5cb47c9c1bbc8c6145819af997b277
Bikram Modi <bikrammodi132@gmail.com>
2026-05-28 21:14:28 +0545

    small change for server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import SERVER from "./server.js";
 import connectDB from "./config/database.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-SERVER.listen(PORT, () => {
+SERVER.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
