0a14fbcb11161e32ae5c764adab9b095ee457315
Bikram Modi <bikrammodi132@gmail.com>
2026-07-22 10:12:40 +0545

    implemented local ai integration

--- src/handlers/index.handler.js
+++ src/handlers/index.handler.js
@@ -2,10 +2,12 @@ import Router from "express";
 
 import USER_ROUTER from "./user.handler.js";
 import AUTH_ROUTER from "./auth.handler.js";
+import AI_ROUTER from "./ai.handler.js";
 
 const HANDLERS = Router();
 
 HANDLERS.use("/users", USER_ROUTER);
 HANDLERS.use("/auth", AUTH_ROUTER);
+HANDLERS.use("/ai", AI_ROUTER);
 
 export default HANDLERS;
